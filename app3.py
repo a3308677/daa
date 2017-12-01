a2e45ff768a22ce8f5c3a77537baad7612761806
5414 <33489932+a3308677@users.noreply.github.com>
2017-12-02 07:21:36 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -36,10 +36,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    line_bot_api.push_message(event.source.userId, TextSendMessage(text='Hello45rld!'))
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-    line_bot_api.push_message(event.source.userId, TextSendMessage(text='Hello45rld!'))
+        TextSendMessage(text='12345'))
+    
 
 import os
 if __name__ == "__main__":
